efd4f047ea6eb26c1da41342812611037fa38a84
Morris Ruschel <morrisruschel@gmail.com>
2022-07-15 21:59:39 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,53 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native'
 
-const HelloWorldApp = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
-  )
+class App extends Component {
+  state = {
+    count: 0
+  }
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+ render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+         style={styles.button}
+         onPress={this.onPress}
+        >
+         <Text>Click me</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>
+            You clicked { this.state.count } times
+          </Text>
+        </View>
+      </View>
+    )
+  }
 }
-export default HelloWorldApp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#FF6',
+    padding: 10,
+    marginBottom: 10
+  }
+})
+
+export default App;
